292f6c21b744be701eaf81ceb3313c36068a68b3
Riley Wilburn <jamesrileywilburn@gmail.com>
2018-09-15 02:54:13 -0400

    mobile: initial test of mobile app

--- mobile/programme/lib/main.dart
+++ mobile/programme/lib/main.dart
@@ -1,20 +1,84 @@
 import 'package:flutter/material.dart';
 
+import 'task.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+      title: 'programme',
+      home: RandomWords(),
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.white,
+        accentColor: Colors.cyan[600],
+        primaryIconTheme: IconThemeData(
+          color:Colors.black,
         ),
-        body: Center(
-          child: Text('Hello World'),
+        textTheme: TextTheme(
+          body2: TextStyle(
+            color: Colors.grey
+          ),
+          title: TextStyle(
+            color: Colors.black,
+          ),
         ),
       ),
     );
   }
 }
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: new IconButton(
+            icon: new Icon(Icons.menu),
+            color: Theme.of(context).primaryIconTheme.color,
+            tooltip: 'Menu',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DayPage()),
+              );
+            }),
+        title: Text("programme", style: Theme.of(context).textTheme.title),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: new TaskList(),
+    );
+  }
+}
+
+class DayPage extends StatefulWidget {
+  @override
+  DayPageState createState() => new DayPageState();
+}
+
+class DayPageState extends State<DayPage> {
+  final String _day = "Today";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: new IconButton(
+          icon: new Icon(Icons.menu),
+          color: Theme.of(context).primaryIconTheme.color,
+          tooltip: 'Air it',
+        ),
+        title: Text(_day, style: Theme.of(context).textTheme.title),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+}
+
---
+++ mobile/programme/lib/task.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:intl/intl.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'dart:math';
+
+var rng = new Random();
+
+var timeFormat = DateFormat('hh:mm a');
+
+class Day {
+  List<ScheduledTask> tasks = new List();
+  DateTime startTime = new DateTime(0, 0, 0, 8);
+
+  Day() {
+    var cur = startTime;
+    for (int i = rng.nextInt(5) + 3; i >= 0; i--) {
+      var next = cur.add(new Duration(minutes: rng.nextInt(60) + 30));
+      tasks.add(new ScheduledTask(cur, next));
+      cur = next;
+    }
+  }
+
+  String dateName() {
+    return "Today";
+  }
+
+  Duration duration() {
+    return tasks.fold(new Duration(seconds: 0),
+        (sum, task) => task.end.difference(task.start) + sum);
+  }
+
+  int stress() {
+    return tasks.fold(0, (sum, task) => task.stress + sum);
+  }
+
+  int score() {
+    var dur = duration();
+    return stress() * dur.inMinutes + dur.inHours * dur.inHours;
+  }
+}
+
+class ScheduledTask {
+  String name = "GENERIC";
+  int stress = 0;
+  DateTime start;
+  DateTime end;
+
+  ScheduledTask(this.start, this.end) {
+    this.name = WordPair.random().asPascalCase;
+    this.stress = rng.nextInt(10);
+  }
+}
+
+class TaskList extends StatefulWidget {
+  @override
+  TaskListState createState() => new TaskListState();
+}
+
+class TaskListState extends State<TaskList> {
+  List<Day> days = new List();
+
+  TaskListState() {
+    for (int i = rng.nextInt(5) + 3; i >= 0; i--) {
+      days.add(new Day());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var rows = days.fold<List<Widget>>(
+        [],
+        (l, day) =>
+            l +
+            ((day.tasks.length == 0) ? [] : [new DayRow(day)]) +
+            day.tasks.map((task) => new TaskRow(task)).toList());
+    return ListView.builder(
+      padding: const EdgeInsets.all(0.0),
+      itemCount: rows.length,
+      itemBuilder: (context, i) {
+        return rows[i];
+      },
+    );
+  }
+}
+
+class DayRow extends StatelessWidget {
+  final Day day;
+
+  DayRow(this.day);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          constraints: BoxConstraints.expand(
+            height: 56.0,
+            width: 10.0,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.purple,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(12.0, 0.0, 0.0, 0.0),
+          child: Text(
+            day.dateName(),
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+        Spacer(),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0.0, 0.0, 16.0, 0.0),
+          child: Row(
+            children: <Widget>[
+              scoreIcon(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  CircleAvatar scoreIcon() {
+    var score = day.score();
+    if (score < 4000) {
+      return CircleAvatar(
+        backgroundColor: Colors.green,
+        child: new Icon(
+          MdiIcons.emoticonHappy,
+          color: Colors.white,
+        ),
+      );
+    } else if (score < 8000) {
+      return CircleAvatar(
+        backgroundColor: Colors.yellow,
+        child: new Icon(
+          MdiIcons.emoticonNeutral,
+          color: Colors.white,
+        ),
+      );
+    }
+    return CircleAvatar(
+      backgroundColor: Colors.red,
+      child: new Icon(
+        MdiIcons.emoticonDead,
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+class TaskRow extends StatefulWidget {
+  final ScheduledTask _task;
+
+  TaskRow(this._task);
+
+  @override
+  TaskRowState createState() => new TaskRowState(this._task);
+}
+
+class TaskRowState extends State<TaskRow> {
+  ScheduledTask _task;
+
+  TaskRowState(this._task);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          constraints: BoxConstraints.expand(
+            height: 56.0,
+            width: 10.0,
+          ),
+          decoration: BoxDecoration(
+            color: this.taskListStressBarColor(),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(12.0, 0.0, 0.0, 0.0),
+          child: Text(
+            this._task.name,
+            style: TextStyle(
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+        Spacer(),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0.0, 0.0, 48.0, 0.0),
+          child: Text(
+            timeFormat.format(this._task.start),
+            style: TextStyle(
+              color: Theme.of(context).textTheme.body2.color,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Color taskListStressBarColor() {
+    if (this._task.stress <= 3) {
+      return Colors.green;
+    } else if (this._task.stress <= 6) {
+      return Colors.orange;
+    }
+    return Colors.red;
+  }
+}
